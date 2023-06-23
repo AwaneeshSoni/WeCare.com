5645e3920f79adbe315a362b450cfb2cfcc0252a
Awaneesh Soni <awaneesh.soni.cer21@iitbhu.ac.in>
2023-06-23 20:59:48 +0530

    Changed the HomePage URL

--- src/components/Sharebuttons.js
+++ src/components/Sharebuttons.js
@@ -5,7 +5,7 @@ import "./privcy.css"
 export default function Sharebutton(props){
     return(
         <div className="aboutus" >
-            <WhatsappShareButton title={props.message} url="https://awaneeshsoni.github.io/wecare.com" >
+            <WhatsappShareButton title={props.message} url="https://we-care-com-by-awaneesh-soni.vercel.app/" >
                 <h4>Share this On WhatsApp with {props.to}</h4>
                 <WhatsappIcon size={50} round={true} />
             </WhatsappShareButton>
